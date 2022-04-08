79a1335fec61d0da5eb71ed5adc735a6529e5d62
nldanne <nldanne97@gmail.com>
2022-04-08 12:54:53 +0200

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import CoinPage from './pages/CoinPage';
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/coins/:id' element={<CoinPage />} />
+      </Routes>
     </div>
   );
 }
