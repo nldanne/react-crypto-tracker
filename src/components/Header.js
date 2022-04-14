fe7318b3932d49697905c5b095f8a003ba86e367
nldanne <nldanne97@gmail.com>
2022-04-14 11:35:10 +0200

    created navbar with currency selector

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { CryptoState } from '../CryptoContext';
+
+import '../styles/Header.scss';
 
 const Header = () => {
+  const { currency, setCurrency } = CryptoState();
+  const [isClicked, setIsClicked] = useState(false);
+
+  const currencyHandler = (event) => {
+    //event.preventDefault();
+    setCurrency(event.target.textContent);
+    setIsClicked(false);
+    //console.log(event.target.textContent);
+  };
+
+
   return (
-    <div>
-      Header
-    </div>
-  )
+    <header>
+      <nav className='navbar'>
+      <h1><Link to='/'>Crypto Ocean</Link></h1>
+      <div className='dropdown'>
+        <div className='dropdown-btn' onClick={() => setIsClicked(!isClicked)}>{currency}</div>
+        {isClicked && (
+          <ol className='dropdown-content'>
+            <li 
+              className='dropdown-item' 
+              key='EUR'
+              onClick={currencyHandler} 
+            >EUR 
+            </li>
+            <li 
+              className='dropdown-item' 
+              key='USD' 
+              onClick={currencyHandler}
+            >USD 
+            </li>
+            <li 
+              className='dropdown-item' 
+              key='HUF' 
+              onClick={currencyHandler}
+            >HUF 
+            </li>
+          </ol>
+        )}
+      </div>
+      </nav>
+
+    </header>
+    )
 }
 
 export default Header;
