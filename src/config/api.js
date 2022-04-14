65a4916a0811d2cdc8d1552ea8dc907677ce3a0b
nldanne <nldanne97@gmail.com>
2022-04-14 18:05:01 +0200

    updated TrendingCoins route

--- src/config/api.js
+++ src/config/api.js
@@ -5,4 +5,4 @@ export const SingleCoin = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
 
 export const TrendingCoins = (currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=4&page=1&sparkline=false&price_change_percentage=24h`;
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=8&page=1&sparkline=false&price_change_percentage=24h`;
