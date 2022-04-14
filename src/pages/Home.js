492969e1fe5f0113c2c79d338c5429f2892e39ea
nldanne <nldanne97@gmail.com>
2022-04-14 11:47:59 +0200

    imported CoinTable

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Banner from '../components/Banner';
-
+import CoinTable from '../components/CoinTable';
 
 const Home = () => {
   return (
     <>
       <Banner />
-
+      <CoinTable />
     </>
   )
 }
